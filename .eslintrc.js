472e879b6eed98edb1a2fdae5bd2cee60d8faa87
raw012 <raw012@ucsd.edu>
2026-04-21 15:31:39 -0700

    Add ESLint configuration file

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'script'
+  },
+  rules: {
+    'no-var': 'error',
+    'eqeqeq': 'error',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'max-len': ['error', { code: 100 }]
+  },
+  overrides: [
+    {
+      files: ['tests/**/*.test.js', 'tests/**/*.spec.js'],
+      env: {
+        jest: true
+      }
+    }
+  ]
+};
